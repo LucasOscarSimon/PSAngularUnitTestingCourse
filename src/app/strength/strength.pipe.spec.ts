46dfc2be411087f3152249ce6052c32c196074b0
Lucas Oscar Simon <lucas.oscar.simon@gmail.com>
2018-11-19 17:01:35 -0300

    More tests

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -5,5 +5,17 @@ describe('StrengthPipe', () => {
     let pipe = new StrengthPipe();
     let val = pipe.transform(5);
     expect(val).toEqual('5 (weak)');
-  })
+  });
+
+  it('should display strong if strength is 10', () => {
+    let pipe = new StrengthPipe();
+    let val = pipe.transform(10);
+    expect(val).toEqual('10 (strong)');
+  });
+
+  it('should display unbelievable if strength is <= 20', () => {
+    let pipe = new StrengthPipe();
+    let val = pipe.transform(21);
+    expect(val).toEqual('21 (unbelievable)');
+  });
 });
