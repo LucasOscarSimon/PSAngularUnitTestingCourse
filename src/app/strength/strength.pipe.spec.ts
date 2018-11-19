b946c97866a46341d83e36ff24b9d1775c2b1394
Lucas Simon <lucas.oscar.simon@gmail.com>
2018-11-19 16:15:28 -0300

    First unit test in Angular

---
+++ src/app/strength/strength.pipe.spec.ts
@@ -0,0 +1,9 @@
+import {StrengthPipe} from "./strength.pipe";
+
+describe('StrengthPipe', () => {
+  it('should display weak if strength is 5', () => {
+    let pipe = new StrengthPipe();
+    let val = pipe.transform(5);
+    expect(val).toEqual('5 (weak)');
+  })
+});
